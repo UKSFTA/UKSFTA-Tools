1e01b4e29b5221622fd724d02a15e7779a395821
Matthew Barker <matthewbarker@librem.one>
2026-02-08 21:28:52 +0000

    fix: Explicitly pass release command in release.sh

--- release.sh
+++ release.sh
@@ -3,5 +3,5 @@
 # UKSFTA Release Shortcut
 # Ensures release builds have corrected timestamps and archives are generated.
 
-# We call build.sh with the 'release' command
+# We call build.sh and force the 'release' command as the first argument
 bash build.sh release "$@"
